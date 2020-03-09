b0d90f8b5d3bfc7adb55f9ab05b5b6d05f7fe638
Hakim-Mohammed <hakim.mohammed@hotmail.fr>
2020-03-09 17:10:12 +0100

    envoi

---
+++ exo-foad/exercice cour 2B/exo2/js/main.js
@@ -0,0 +1,16 @@
+function changer_style(){
+    var p = document.getElementById('parag1');
+    p.style.color="white";
+    p.style.backgroundColor="red";
+    p.style.border="2px black dotted";
+    p.style.padding="5px";
+
+  }
+
+// function changer_style(){
+
+//     var p = document.getElementById('parag1');
+//     p.classList.add("active");
+
+//   }
+
