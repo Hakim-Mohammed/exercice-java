4c858166209e17227710338c649a978ad3c0ca51
Hakim-Mohammed <hakim.mohammed@hotmail.fr>
2020-03-17 18:09:08 +0100

    correction foad 16/03

--- exo-foad/exercice cour 2C/exo6/js/main.js
+++ exo-foad/exercice cour 2C/exo6/js/main.js
@@ -1,13 +1,31 @@
-function longestWord(string) {
-    let str = string.split(" ");
-    let longest = 0;
-    let word = null;
-    str.forEach(function(str) {
-        if (longest < str.length) {
-            longest = str.length;
-            word = str;
+// function longestWord(string) {
+//     let str = string.split(" ");
+//     let longest = 0;
+//     let word = null;
+//     str.forEach(function(str) {
+//         if (longest < str.length) {
+//             longest = str.length;
+//             word = str;
+//         }
+//     });
+//     return word;
+// }
+// console.log(longestWord("Tutorial Développement Web"));
+
+// **************************************************
+
+function findLongestWord(str){
+    let array1 = str.match(/\w[a-z\u00E0-\u00FC]{0,}/gi);
+    // document.write(array1);
+    let result = array1[0];
+    document.write(result);
+
+    for ( let x=1 ; x < array1.length ; x++){
+        if(result.length < array1[x].length){
+            result = array[x]
         }
-    });
-    return word;
+    }
+    return result;
 }
-console.log(longestWord("Tutorial Développement Web"));
+// console.log(findLongestWord('Tutorial Développement Web'));
+document.write(findLongestWord('Tutorial Développement Web'));
